2aab7e1e8e8f6e9b8b05d2db7712a2cce55ebe25
Yuki Kodama <kodama@jvr.jp>
2014-11-26 06:32:36 +0000

    Ignore binary data

--- lib/mail-unf/patches.rb
+++ lib/mail-unf/patches.rb
@@ -3,7 +3,9 @@ require 'mail'
 module Mail
   class Message
     def process_body_raw_with_unf
-      @body_raw = @body_raw.to_s.to_nfc
+      unless @body_raw.encoding.to_s.upcase == 'ASCII-8BIT'
+        @body_raw = @body_raw.to_s.to_nfc
+      end
       process_body_raw_without_unf
     end
     alias_method :process_body_raw_without_unf, :process_body_raw
